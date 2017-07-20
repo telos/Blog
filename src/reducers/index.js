203677f462e05b74c019ee13b9ea46918a08ca05
Jacob Parma <jacobparma@gmail.com>
2017-07-20 15:15:39 -0500

    S9-131 Redux Form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
+import { reducer as formReducer } from 'redux-form';
 import PostsReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
+  form: formReducer,
   posts: PostsReducer
 });
 
