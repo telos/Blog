ddb55f60365b21b5d8eeb34fcf4e911a5e01fd5d
Jacob Parma <jacobparma@gmail.com>
2017-07-20 15:08:56 -0500

    S9-130 Navigation with the Link Component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
+import { Link } from 'react-router-dom';
 
 import { fetchPosts } from '../actions/index';
 
@@ -24,6 +25,11 @@ class PostsIndex extends Component {
     console.log(this.props.posts);
     return (
       <div>
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a post
+          </Link>
+        </div>
         <h3>Posts</h3>
         <ul className="list-group">
           { this.renderPosts() }
