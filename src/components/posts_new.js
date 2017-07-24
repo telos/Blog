eb8b60dc4674182107be97322e59f99d4899a6a1
Jacob Parma <jacobparma@gmail.com>
2017-07-24 16:33:23 -0500

    S9-141 Create Post Action Creator

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
+
 class PostsNew extends Component {
   renderField(field) {
     const { meta: { touched, error } } = field;
@@ -23,8 +26,7 @@ class PostsNew extends Component {
   }
 
   onSubmit(values) {
-    // this === component
-    console.log(values);
+    this.props.createPost(values);
   }
 
   render() {
@@ -77,4 +79,6 @@ function validate(values) {
 export default reduxForm({
   validate: validate,
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
