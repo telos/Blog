2dd1f89f3d140d2b5afc1bd607dba9ac73e7036a
Jacob Parma <jacobparma@gmail.com>
2017-07-24 22:59:58 -0500

    S9-144 Receiving New Posts

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,20 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST } from '../actions/index';
 
 export default function(state = {}, action) {
   switch(action.type) {
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
+    case FETCH_POST:
+      // ES5 syntax
+      // const post = action.payload.data;
+      // const newState = { ...state };
+      // newState[post.id] = post;
+      // return newState;
+
+      // ES6 syntax, brackets are using key interpolation to define the new key
+      return { ...state, [action.payload.data.id]: action.payload.data };
     default:
       return state;
   }
