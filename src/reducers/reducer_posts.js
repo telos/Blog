11599a892b3f5d028f3dca5a2c36492b08c684bd
Jacob Parma <jacobparma@gmail.com>
2017-07-24 23:37:36 -0500

    S9-148 Deleting a Post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-import { FETCH_POSTS, FETCH_POST } from '../actions/index';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/index';
 
 export default function(state = {}, action) {
   switch(action.type) {
