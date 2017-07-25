c9b4d50f7b1355c5336b7aaf7b1a1f59d4dec9c7
Jacob Parma <jacobparma@gmail.com>
2017-07-24 23:45:41 -0500

    S9-148-2 Cleanup Post Deleteion State

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,6 +4,8 @@ import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions/index';
 
 export default function(state = {}, action) {
   switch(action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload);
     case FETCH_POSTS:
       return _.mapKeys(action.payload.data, 'id');
     case FETCH_POST:
